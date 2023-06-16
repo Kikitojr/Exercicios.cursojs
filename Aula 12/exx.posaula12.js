4841af2c49cc12833508eec9931f3ab9ce34712a
Kikitojr <130502687+Kikitojr@users.noreply.github.com>
2023-06-15 21:15:57 -0300

    Delete Aula 12 directory

--- Aula 12/exx.posaula12.js
+++
@@ -1,8 +0,0 @@
-console.log (`2x2 é quanto?`)
-var resultado = 7
-console.log (`O valor digitado foi ${resultado}!`)
-if (resultado == 7){
-    console.log (`Correto!`)
-}else{
-    console.log(`Errado!`)
-}
